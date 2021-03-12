ef44d890baf650788396a885182af8dc7aac6599
michelle <laptopradiodj@gmail.com>
2021-03-12 11:45:08 -0800

    fix process.env

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,19 +5,12 @@
  */
 
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`
+    path: `.env.${process.env.NODE_ENV}`
 })
 
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    {
-      resolve: 'gatsby-source-stripe',
-      options: {
-        objects: ["Price"],
-        secretKey: process.env.STRIPE_SECRET_KEY,
-        downloadFiles: true
-      }
-    }
-  ],
+    /* Your site config here */
+    plugins: [
+
+    ],
 }
