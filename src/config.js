f8ff7172c55e256778ec48986b9b3127d5934617
Amy Hua <amy@yourbase.io>
2021-03-24 20:12:29 -0700

    Update config vars

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-  GALLERY_NAME: process.env.GALLERY_NAME,
-  IFRAME_EMBED_LINK: process.env.IFRAME_EMBED_LINK || 'https://opensea.io/collection/superchief-gallery-nft?embed=true&default=true',
-  CONTACT_EMAIL: 'superchiefgallerynft@gmail.com',
-  AIRTABLE_EMBED_SRC: process.env.AIRTABLE_EMBED_SRC || 'https://airtable.com/embed/shrfdT9wSh4jZWuNy?backgroundColor=gray&default=true'
+  GALLERY_NAME: process.env.GATSBY_GALLERY_NAME,
+  IFRAME_EMBED_LINK: process.env.GATSBY_IFRAME_EMBED_LINK || 'https://opensea.io/collection/superchief-gallery-nft?embed=true&default=true',
+  CONTACT_EMAIL: process.env.GATSBY_CONTACT_EMAIL || 'superchiefgallerynft@gmail.com',
+  AIRTABLE_EMBED_SRC: process.env.GATSBY_AIRTABLE_EMBED_SRC || 'https://airtable.com/embed/shrfdT9wSh4jZWuNy?backgroundColor=gray&default=true'
 }
 
 export default config;
