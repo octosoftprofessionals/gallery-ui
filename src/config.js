7cad83652e7b66610c0a3d49de75c65e38505400
Amy Hua <amy@yourbase.io>
2021-03-25 20:08:30 -0700

    Update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   GALLERY_NAME: process.env.GATSBY_GALLERY_NAME || 'Superchief Gallery',
-  MARKETPLACE_IFRAME_EMBED_LINK: process.env.GATSBY_MARKETPLACE_IFRAME_EMBED_LINK || 'https://opensea.io/collection/superchief-gallery-nft?embed=true',
+  MARKETPLACE_IFRAME_EMBED_LINK: process.env.GATSBY_MARKETPLACE_IFRAME_EMBED_LINK || 'https://opensea.io/collection/superchief-gallery-nifty?embed=true',
   CONTACT_EMAIL: process.env.GATSBY_CONTACT_EMAIL || 'superchiefgallerynft@gmail.com',
   AIRTABLE_EMBED_SRC: process.env.GATSBY_AIRTABLE_EMBED_SRC || 'https://airtable.com/embed/shrfdT9wSh4jZWuNy?backgroundColor=gray',
 }
