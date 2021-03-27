35e7de130d73a6dcc9462fd0cb5ccbc931d2b6fe
Amy Hua <amy@yourbase.io>
2021-03-27 13:37:21 -0700

    Submit art -> buy timed collectibles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -23,9 +23,10 @@ const Layout: React.FC = ({ children }) => {
       >
         <div className={classes.navbarContent}>
           <Link className={classes.logo} to={config.LOGO_LINK}>
-            <img src={logoSrc} style={{height: 30}} />
+            <img src={logoSrc} style={{height: 26}} />
           </Link>
           <span className={classes.socialLinks}>
+            <Link className={classes.submitLink} to="https://opensea.io/assets/superchief-gallery-nifty?search%5BresultModel%5D=ASSETS&search%5BsortAscending%5D=false&search%5Btoggles%5D%5B0%5D=BUY_NOW">BUY TIMED COLLECTIBLES</Link>
             <span className={classes.socialIcons}>
               <SocialIcons />
             </span>
