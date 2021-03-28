2ce5dffef8087a391a3bf852e339468df0987805
Amy Hua <amy@yourbase.io>
2021-03-27 19:54:45 -0700

    Fix links

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -22,11 +22,11 @@ const Layout: React.FC = ({ children }) => {
         innerStyle={{ padding: 0 }}
       >
         <div className={classes.navbarContent}>
-          <Link className={classes.logo} to={config.LOGO_LINK}>
+          <Link className={classes.logo} to="/">
             <img src={logoSrc} style={{height: 26}} />
           </Link>
           <span className={classes.socialLinks}>
-            <Link className={classes.submitLink} to="https://opensea.io/assets/superchief-gallery-nifty?search%5BresultModel%5D=ASSETS&search%5BsortAscending%5D=false&search%5Btoggles%5D%5B0%5D=BUY_NOW">BUY TIMED COLLECTIBLES</Link>
+            <Link className={classes.submitLink} to="/collectibles">BUY TIMED COLLECTIBLES</Link>
             <span className={classes.socialIcons}>
               <SocialIcons />
             </span>
