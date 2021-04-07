0cede394481c5510da32e3ce1bd4fdc077c5ffe2
Jeremy Meyer <jeremypatrickmeyer@gmail.com>
2021-04-06 21:30:23 -0700

    fixup code style in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,30 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-require("dotenv").config({
-    path: `.env.${process.env.NODE_ENV}`
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
 })
 
 module.exports = {
-    /* Your site config here */
-    plugins: [
-        'gatsby-plugin-styled-components',
-        'gatsby-plugin-typescript',
-    ],
+  /* Your site config here */
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-theme-material-ui',
+      options: {
+        webFontsConfig: {
+          fonts: {
+            google: [
+              {
+                family: 'Bai Jamjuree Regular',
+                variants: ['200', '300', '400', '500', '600', '700'],
+                // variants: ['400', '500', '600', '900'],
+              },
+            ],
+          },
+        },
+      },
+    },
+  ],
 }
