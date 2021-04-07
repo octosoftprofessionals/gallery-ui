b98cee445e2ceef2e4ac0bc444dd24a68e308696
Jeremy Meyer <jeremypatrickmeyer@gmail.com>
2021-04-06 17:21:30 -0700

    refactor Artwork/Show

--- src/pages/artwork/show/index.jsx
+++ src/pages/artwork/show/index.jsx
@@ -1,95 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import ArtworkDisplay from '../../components/ArtworkDisplay'
-import Carousel from '../../components/Carousel'
-import Layout from '../../components/Layout/Layout'
+import ArtworkDisplay from '../../../components/ArtworkDisplay'
+import Layout from '../../../components/Layout/Layout'
+
+import Hero from './Hero'
+import MoreItems from './MoreItems'
+import SmartDescription from './SmartDescription'
 
 const imgUrl = 'https://image.mux.com/OqOt4fV1UKU02PntGC022luD9O7J01JZ701etlf022JIhd6A/thumbnail.jpg'
 const videoUrl = 'http://localhost:8000/float.mp4'
 const title = 'unnamed'
 const artist = '@jandali'
-const descriptionLines = [
+const descriptionParagraphs = [
   'This is the real deal.',
   'Created by fire and light, it showcases the power of all unknown mysteries witheld.',
   'The night is come, but not too soon;\nAnd sinking silently,\nAll silently, the little moon\nDrops down behind the sky.\nThere is no light in earth or heaven\nBut the cold light of stars;\nAnd the first watch of night is given\nTo the red planet Mars.',
 ]
 const value = '50.00 ETH'
 
-const Hero = ({ centerpiece }) => (
-  <HeroRoot>
-    <HeroAssetContainer>
-      {centerpiece}
-    </HeroAssetContainer>
-  </HeroRoot>
-)
-
-const HeroRoot = styled.div`
-  margin-top: 78px;
-  background-color: antiquewhite;
-  padding: 60px 24px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const HeroAssetContainer = styled.div`
-  width: 50%;
-  max-width: 520px;
-`;
-
-const ThickDivider = () => (
-  <div style={{ borderBottom: '2px solid #333', marginTop: '1.25em', marginBottom: '1.5em' }}></div>
-)
-
-const items = [...new Array(5)].map(() => ({}))
-
-const Description = ({ lines }) => (
-  <DescriptionRoot>
-    {descriptionLines.map(line => (
-      <p>
-        {line.split('\n').map((newline, index, ary) => (
-          index === ary.length - 1 ?
-            newline :
-            <>
-              {newline}<br />
-            </>
-        ))}
-      </p>
-    ))}
-  </DescriptionRoot>
-)
-
-const DescriptionRoot = styled.div`
-  margin-top: 32px;
-`;
-
-const MoreItems = () => (
-  <MoreItemsRoot>
-    <MoreItemsHeader>
-      <h2>Browse More Artwork</h2>
-      <ThickDivider />
-    </MoreItemsHeader>
-    <CarouselContainer>
-      <Carousel items={items} />
-    </CarouselContainer>
-  </MoreItemsRoot>
-)
-
-const MoreItemsRoot = styled.div`
-  width: 50%;
-`;
-
-const MoreItemsHeader = styled.div`
-  width: 100%;
-`;
-
-const CarouselContainer = styled.div`
-  width: 100%;
-`;
-
-const params = { imgUrl, videoUrl, title, artist, descriptionLines, value }
-
+// params: { imgUrl, videoUrl, title, artist, descriptionParagraphs, value }
 const ShowArtwork = () => {
   return (
     <div>
@@ -112,7 +42,7 @@ const ShowArtwork = () => {
 
             <Divider />
 
-            <Description lines={descriptionLines} />
+            <SmartDescription paragraphs={descriptionParagraphs} />
           </ContentContainer>
 
           <MoreItems />
