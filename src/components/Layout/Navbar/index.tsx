1bc613328dfa065052ab32b977496fc0d4bdc939
Roger BC <diolink.4103@gmail.com>
2021-04-07 21:27:29 -0300

    added drawer in mobile view

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import { makeStyles } from '@material-ui/core/styles'
+import MenuIcon from '@material-ui/icons/Menu'
+import HighlightOffIcon from '@material-ui/icons/HighlightOff'
 
 import logoSrc from '../../../assets/logo.png'
 
@@ -10,14 +12,17 @@ import {
   Toolbar,
   Typography,
   Button,
-  ButtonGroup,
+  withWidth,
+  Hidden,
+  IconButton,
+  SwipeableDrawer,
 } from '@material-ui/core'
 
 const useStyles = makeStyles({
   root: { padding: '32px 0 0' },
   img: {
     background: `url(${logoSrc})`,
-    paddingBottom: '10vh',
+    paddingBottom: 55,
     backgroundPosition: 'center',
     backgroundSize: 'contain',
     backgroundRepeat: 'no-repeat',
@@ -38,58 +43,163 @@ const useStyles = makeStyles({
     backgroundColor: '#000',
     color: '#fff',
   },
+  buttonMenu: { boxShadow: '0px 10px 20px rgb(0 0 0 / 5%)' },
+  link: {
+    textDecoration: 'none',
+    cursor: 'pointer',
+  },
+  drawer: { width: '100vw', padding: 24 },
+  offIcon: { fontSize: '3em' },
+  imgMenu: {
+    marginLeft: 12,
+    marginTop: 20,
+    background: `url(${logoSrc})`,
+    paddingBottom: 55,
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+  },
 })
 
 const index = () => {
   const classes = useStyles({ logoSrc })
+  const [showDrawer, setShowDrawer] = useState(false)
   return (
     <>
       <AppBar position="static" color="transparent" elevation={0}>
         <Toolbar className={classes.root}>
           <Grid container justify="space-between" alignItems="center">
             <Grid item xs={2}>
-              <Link to="/">
+              <Link to="/" className={classes.link}>
                 <div className={classes.img} />
               </Link>
             </Grid>
 
             <div className={classes.container}>
-              <Button variant="text" className={classes.button}>
-                <Typography variant="button" className={classes.textButton}>
-                  Artworks
-                </Typography>
-              </Button>
-              <Button
-                variant="text"
-                className={[classes.button, classes.selected]}
-              >
-                <Typography variant="button" className={classes.textButton}>
-                  Home
-                </Typography>
-              </Button>
-              <Button variant="text" className={classes.button}>
-                <Typography variant="button" className={classes.textButton}>
-                  Creator
-                </Typography>
-              </Button>
+              <Hidden mdDown>
+                <Link to="/" className={classes.link}>
+                  <Button variant="text" className={classes.button}>
+                    <Typography variant="button" className={classes.textButton}>
+                      Artworks
+                    </Typography>
+                  </Button>
+                </Link>
+                <Link to="/" className={classes.link}>
+                  <Button
+                    variant="text"
+                    className={[classes.button, classes.selected]}
+                  >
+                    <Typography variant="button" className={classes.textButton}>
+                      Home
+                    </Typography>
+                  </Button>
+                </Link>
+                <Link to="/" className={classes.link}>
+                  <Button variant="text" className={classes.button}>
+                    <Typography variant="button" className={classes.textButton}>
+                      Creator
+                    </Typography>
+                  </Button>
+                </Link>
+              </Hidden>
             </div>
 
-            <Grid item>
-              <Button
-                variant="contained"
-                color="primary"
-                className={classes.buttonWallet}
-              >
-                <Typography variant="button" className={classes.textButton}>
-                  Connect Wallet
-                </Typography>
-              </Button>
-            </Grid>
+            <Hidden smDown>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.buttonWallet}
+                >
+                  <Typography variant="button" className={classes.textButton}>
+                    Connect Wallet
+                  </Typography>
+                </Button>
+              </Grid>
+            </Hidden>
           </Grid>
+          <Hidden mdUp>
+            <IconButton
+              color="inherit"
+              className={classes.buttonMenu}
+              aria-label="open drawer"
+              size="medium"
+              onClick={() => setShowDrawer(true)}
+              edge="end"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
         </Toolbar>
       </AppBar>
+
+      <Hidden mdUp>
+        <SwipeableDrawer
+          anchor={'right'}
+          open={showDrawer}
+          onOpen={() => setShowDrawer(true)}
+        >
+          <Grid
+            container
+            justify="space-between"
+            alignItems="flex-start"
+            className={classes.drawer}
+          >
+            <Grid
+              item
+              container
+              justify="flex-start"
+              direction="column"
+              xs={6}
+              spacing={5}
+            >
+              <Grid item>
+                <Link to="/" className={classes.link}>
+                  <div className={classes.imgMenu} />
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link className={classes.link}>
+                  <Typography variant="h4" className={classes.textButton}>
+                    Artworks
+                  </Typography>
+                </Link>
+                <Link className={classes.link}>
+                  <Typography variant="h4" className={classes.textButton}>
+                    Creators
+                  </Typography>
+                </Link>
+                <Link className={classes.link}>
+                  <Typography variant="h4" className={classes.textButton}>
+                    Blog
+                  </Typography>
+                </Link>
+                <Link className={classes.link}>
+                  <Typography variant="h4" className={classes.textButton}>
+                    Help
+                  </Typography>
+                </Link>
+                <Link className={classes.link}>
+                  <Typography variant="h4" className={classes.textButton}>
+                    Discord
+                  </Typography>
+                </Link>
+                <Link className={classes.link}>
+                  <Typography variant="h4" className={classes.textButton}>
+                    Careers
+                  </Typography>
+                </Link>
+              </Grid>
+            </Grid>
+            <Grid item xs={2}>
+              <IconButton color="inherit" onClick={() => setShowDrawer(false)}>
+                <HighlightOffIcon className={classes.offIcon} />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </SwipeableDrawer>
+      </Hidden>
     </>
   )
 }
 
-export default index
+export default withWidth()(index)
