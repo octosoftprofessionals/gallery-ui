3ffc5ffb674eb72e98dd12851247edbefee4a927
Roger BC <diolink.4103@gmail.com>
2021-04-16 22:50:37 -0300

    countdown is implemented

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,6 +2,28 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
+const videoUrls = ['http://localhost:8000/float.mp4']
+
+const imgUrls = [
+  'https://f8n-ipfs-production.imgix.net/Qme7ShWfH2GHnbKHo9Vb41PxMwLunLxgKGebF94RzjGhCs/nft.png',
+  'https://cdn.cultofmac.com/wp-content/uploads/2011/10/youngstevejobs.jpg',
+  'https://f8n-ipfs-production.imgix.net/QmTf4rxGkyryv6Vnm9mFJxWTEXcqjmtgxQXz7m5cqmLFsv/nft.jpg',
+  'https://f8n-ipfs-production.imgix.net/QmeFJYbYeN6cfojypwzyAUYNyDFxFUD5tvjTG23LEF6xNY/nft.jpg',
+  'https://f8n-ipfs-production.imgix.net/Qme6A7qARnvZsn5RNSuJS8MyZjzzev4afcr6JVJxjciUvB/nft.png',
+  // 'https://image.mux.com/OqOt4fV1UKU02PntGC022luD9O7J01JZ701etlf022JIhd6A/thumbnail.jpg',
+]
+
+const start = new Date('2021/04/16 11:43:00 PM')
+
+const randAsset = () => {
+  const assetUrls = imgUrls.concat(videoUrls)
+  return assetUrls[Math.floor(Math.random() * assetUrls.length)]
+}
+
+const randImg = () => {
+  return imgUrls[Math.floor(Math.random() * imgUrls.length)]
+}
+
 import GalleryItem from './GalleryItem'
 
 const useStyle = makeStyles(Theme => ({
@@ -12,22 +34,28 @@ const useStyle = makeStyles(Theme => ({
 const Gallery = ({ items }) => {
   const classes = useStyle()
   return (
-    <>
-      <Grid container direction="row" justify="space-around" wrap="wrap">
-        {items.map((item, index) => (
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            lg={3}
-            className={classes.containerItem}
-          >
-            <GalleryItem key={index} {...item} />
-          </Grid>
-        ))}
-      </Grid>
-    </>
+    <Grid container direction="row" justify="space-around" wrap="wrap">
+      {items.map((item, index) => (
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          lg={3}
+          className={classes.containerItem}
+        >
+          <GalleryItem
+            key={index}
+            {...item}
+            imgUrl={randImg()}
+            price={'5.00'}
+            artis={'@ArtisName'}
+            titleArt={'Unname'}
+            start={start}
+          />
+        </Grid>
+      ))}
+    </Grid>
   )
 }
 
--- src/components/GalleryItem.tsx
+++ src/components/GalleryItem.tsx
@@ -1,34 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { Avatar, Grid, Paper, Typography } from '@material-ui/core'
 
 import ArtworkDisplay from './ArtworkDisplay'
-import { title } from 'node:process'
-
-const videoUrls = ['http://localhost:8000/float.mp4']
-
-const imgUrls = [
-  'https://f8n-ipfs-production.imgix.net/Qme7ShWfH2GHnbKHo9Vb41PxMwLunLxgKGebF94RzjGhCs/nft.png',
-  'https://cdn.cultofmac.com/wp-content/uploads/2011/10/youngstevejobs.jpg',
-  'https://f8n-ipfs-production.imgix.net/QmTf4rxGkyryv6Vnm9mFJxWTEXcqjmtgxQXz7m5cqmLFsv/nft.jpg',
-  'https://f8n-ipfs-production.imgix.net/QmeFJYbYeN6cfojypwzyAUYNyDFxFUD5tvjTG23LEF6xNY/nft.jpg',
-  'https://f8n-ipfs-production.imgix.net/Qme6A7qARnvZsn5RNSuJS8MyZjzzev4afcr6JVJxjciUvB/nft.png',
-  // 'https://image.mux.com/OqOt4fV1UKU02PntGC022luD9O7J01JZ701etlf022JIhd6A/thumbnail.jpg',
-]
-
-const randAsset = () => {
-  const assetUrls = imgUrls.concat(videoUrls)
-  return assetUrls[Math.floor(Math.random() * assetUrls.length)]
-}
-
-const randImg = () => {
-  return imgUrls[Math.floor(Math.random() * imgUrls.length)]
-}
+import { deltaTime, timeFormat } from '../Utils'
 
 const useStyle = makeStyles(Theme => ({
   root: {},
   img: {
-    backgroundImage: props => `url(${props.imgUrls})`,
+    backgroundImage: props => `url(${props.imgUrl})`,
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
@@ -45,70 +25,79 @@ const useStyle = makeStyles(Theme => ({
   containerAvatar: { marginTop: Theme.spacing(9) },
 }))
 
-const GalleryItem = ({}) => {
-  const classes = useStyle({ imgUrls: randImg() })
-  const price = '5.00'
-  const hour = 12
-  const min = 21
-  const sec = 45
-  const artis = '@ArtisName'
-  const title = 'Unname'
+const GalleryItem = ({ imgUrl, price, artis, titleArt, start }) => {
+  const [timer, setTimer] = useState('')
+
+  const classes = useStyle({ imgUrl: imgUrl })
+
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      const delta = deltaTime(start)
+      if (delta >= 0) {
+        setTimer(timeFormat(delta))
+      } else {
+        clearInterval(timeInterval)
+        setTimer('Auction has ended')
+      }
+    }, 1000)
+  }, [])
+
   return (
-    <>
-      <Paper variant="elevation" elevation={1} className={classes.root}>
-        <div className={classes.img} />
+    <Paper variant="elevation" elevation={1} className={classes.root}>
+      <div className={classes.img} />
+      <Grid
+        item
+        container
+        direction="column"
+        alignItems="center"
+        justify="space-around"
+      >
         <Grid
           item
+          xs={10}
           container
-          direction="column"
-          alignItems="center"
-          justify="space-around"
+          justify="flex-start"
+          className={classes.infoCard}
         >
+          <Typography variant="h5" color="initial">
+            {titleArt}
+          </Typography>
           <Grid
             item
-            xs={10}
+            xs={12}
             container
-            justify="flex-start"
-            className={classes.infoCard}
+            direction="row"
+            alignItems="center"
+            className={classes.containerAvatar}
           >
-            <Typography variant="h5" color="initial">
-              {title}
+            <Avatar alt="avat" src={`${imgUrl}`} />
+            <Typography variant="subtitle1" color="initial">
+              {artis}
             </Typography>
-            <Grid
-              item
-              xs={12}
-              container
-              direction="row"
-              alignItems="center"
-              className={classes.containerAvatar}
-            >
-              <Avatar alt="avat" src={`${randImg()}`} />
-              <Typography variant="subtitle1" color="initial">
-                {artis}
-              </Typography>
-            </Grid>
           </Grid>
-          <Grid item container className={classes.footerCard}>
-            <Grid item xs={6} container>
-              <Typography variant="caption" className={classes.titels}>
-                Current bid
-              </Typography>
-              <Typography variant="caption" color="secondary">
-                {`${price} ETH`}
-              </Typography>
-            </Grid>
-            <Grid item xs={6} container alignItems="flex-start">
+        </Grid>
+        <Grid item container className={classes.footerCard}>
+          <Grid item xs={6} container>
+            <Typography variant="caption" className={classes.titels}>
+              Current bid
+            </Typography>
+            <Typography variant="caption" color="secondary">
+              {`${price} ETH`}
+            </Typography>
+          </Grid>
+          <Grid item xs={6} container alignItems="flex-start">
+            <Grid item xs={12}>
               <Typography variant="caption" className={classes.titels}>
                 Ending in
               </Typography>
-              <Typography variant="caption" color="secondary">
-                {`${hour}h ${min}m ${sec}s`}
-              </Typography>
             </Grid>
+            <Typography variant="caption" color="secondary">
+              {timer}
+            </Typography>
           </Grid>
         </Grid>
-      </Paper>
-    </>
+      </Grid>
+    </Paper>
   )
 }
 
