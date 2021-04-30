2c2a817f627b51d062024a264b1a8f43cd739bc9
Agustina <agustina@blockinar.com>
2021-04-30 15:49:11 -0300

    artworkShow: add creator button component

--- src/components/CreatorButton.tsx
+++ src/components/CreatorButton.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CreatorButton = ({url_profile, user, link}) =>{
+const CreatorButton = ({imgUrl, user, link}) =>{
     return(
             <ButtonCreator>
                 <LinkUser  id="" href={link}>
                     <ImgConteiner className='creator-button-img-cont'>
-                        <ImgUser src={url_profile} alt={user}/>
+                        <ImgUser src={imgUrl} alt={user}/>
                     </ImgConteiner>
                     <User>
-                        @{user}
+                        {user}
                     </User>
                 </LinkUser>
             </ButtonCreator>
@@ -21,24 +21,38 @@ export default CreatorButton;
 
 const User = styled.p`
     margin:0;
-    font-family: Avenir, Avenir Next, Helvetica Neue, 'Bai Jamjuree Bold';
+    font-family:'Bai Jamjuree';
     font-style:normal;
-    font-size: 16px;
+    font-weight: bold;
+    font-size: 14px;
     color:#1F1F1F;
+
+    @media (min-width: 600px) {
+        font-size: 16px;
+    }
 `
 const ButtonCreator = styled.button`
-    padding: 11px;
+    padding: 8px;
     padding-right: 20px;
-    height:56px;
+    height:40px;
     background-color: white;
     border:none;
     border-radius:30px;
     box-shadow: 0px 10px 20px rgb(0 0 0 / 5%);
     transition: all 300ms cubic-bezier(0.23,1,0.32,1);
     will-change: transform;
+    position:absolute;
+    top: 83%;
+    margin-left:24px;
     :hover{
         box-shadow: 0px 10px 20px rgb(0 0 0 / 10%);
         transform: translateY(-2px);
+    }
+    @media (min-width: 600px) {
+        padding: 11px;
+        padding-right: 20px;
+        height:56px;
+        top: 83%;
     }
 `
 const ImgUser = styled.img`
@@ -46,16 +60,19 @@ const ImgUser = styled.img`
     width:34px;
 `
 const ImgConteiner = styled.div`
-    width:34px;
-    height: 34px;
+    width: 24px;
+    height: 24px;
     border-radius:50%;
     overflow: hidden;
     margin-right: 10px;
+    @media (min-width: 600px) {
+        width:34px;
+        height: 34px;
+    }
 `
 
 const LinkUser = styled.a`
     font-family: Avenir, Avenir Next, Helvetica Neue, 'Bai Jamjuree Bold';
-    width:120px;
     display:flex;
     justify-content:center;
     align-items:center;
