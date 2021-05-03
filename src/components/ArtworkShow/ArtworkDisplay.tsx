14032522e3ea6b17dc6d7204e2ae3f13e325ec5e
Roger BC <diolink.4103@gmail.com>
2021-05-02 23:07:24 -0300

    react-fullScreen is implemented

--- src/components/ArtworkShow/ArtworkDisplay.tsx
+++ src/components/ArtworkShow/ArtworkDisplay.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
+import { FullScreen, useFullScreenHandle } from 'react-full-screen'
 import { Grid, IconButton } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -18,16 +19,19 @@ const useStyle = makeStyles(Theme => ({
     padding: Theme.spacing(9),
   },
   video: { maxWidth: '50vh', filter: boxShadow.boxShadow2, cursor: 'zoom-in' },
-  fullScreen: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: 999,
-    backgroundColor: Theme.palette.secondary.main,
+  fullScreenVideo: {
+    display: 'flex',
+    width: '90vw',
+    height: '90vh',
   },
-  fullScreenVideo: { width: '90vw', height: '90vh' },
+  fullScreen: {
+    backgroundColor: '#fff',
+    display: 'flex',
+  },
+  normalScreen: {
+    display: 'block',
+    backgroundColor: Theme.palette.secondary.light,
+  },
   icon: { fontSize: Theme.spacing(8) },
   iconButton: {
     margin: Theme.spacing(2),
@@ -42,19 +46,16 @@ const ArtworkDisplay = ({ imgUrl, videoUrl, mimeType }) => {
   const [volume, setVolume] = useState<boolean>(true)
   const [fullscreen, setFullscreen] = useState<boolean>(false)
   const classes = useStyle()
-
+  const handle = useFullScreenHandle()
   const toggleFullScreen = () => {
     setFullscreen(!fullscreen)
     if (fullscreen) {
-      if (document.body.requestFullscreen) {
-        document.body.requestFullscreen()
-      }
+      handle.exit()
     } else {
-      if (document.body.webkitRequestFullScreen) {
-        document.body.webkitRequestFullScreen()
-      }
+      handle.enter()
     }
   }
+  console.log('fullscreen :>> ', fullscreen)
 
   const toggleMute = () => {
     setVolume(!volume)
@@ -69,47 +70,55 @@ const ArtworkDisplay = ({ imgUrl, videoUrl, mimeType }) => {
       container
       justify="center"
       direction="column"
-      className={fullscreen ? classes.fullScreen : classes.root}
+      className={classes.root}
     >
-      <Grid item container justify="center">
-        <video
-          poster={imgUrl}
-          src={videoUrl}
-          autoPlay={true}
-          loop={true}
-          onClick={() => toggleFullScreen()}
-          className={fullscreen ? classes.fullScreenVideo : classes.video}
-          ref={videoRef}
-        >
-          <source
-            src={
-              'https://fnd.fleek.co/fnd-prod/QmeKXDYWUvL42en8vH8rz9UYyvLDY71N1ZbRYhK9MHgUDA/nft.mp4'
-            }
-            type={mimeType}
-          />
-          <img src={imgUrl} />
-        </video>
-      </Grid>
-      <Grid item container justify="flex-end" className={classes.buttons}>
-        <IconButton
-          onClick={() => toggleFullScreen()}
-          className={classes.iconButton}
-        >
-          {fullscreen ? (
-            <Fullscreen color="secondary" className={classes.icon} />
-          ) : (
-            <FullscreenExit color="secondary" className={classes.icon} />
-          )}
-        </IconButton>
+      <FullScreen
+        handle={handle}
+        className={fullscreen ? classes.fullScreen : classes.normalScreen}
+      >
+        <Grid container justify="center" alignItems="center">
+          <video
+            poster={imgUrl}
+            src={videoUrl}
+            autoPlay={true}
+            loop={true}
+            onClick={handle.enter}
+            className={fullscreen ? classes.fullScreenVideo : classes.video}
+            ref={videoRef}
+          >
+            <source
+              src={
+                'https://fnd.fleek.co/fnd-prod/QmeKXDYWUvL42en8vH8rz9UYyvLDY71N1ZbRYhK9MHgUDA/nft.mp4'
+              }
+              type={mimeType}
+            />
+            <img src={imgUrl} />
+          </video>
+        </Grid>
+        <Grid item container justify="flex-end" className={classes.buttons}>
+          <IconButton
+            onClick={() => toggleFullScreen()}
+            className={classes.iconButton}
+          >
+            {fullscreen ? (
+              <Fullscreen color="secondary" className={classes.icon} />
+            ) : (
+              <FullscreenExit color="secondary" className={classes.icon} />
+            )}
+          </IconButton>
 
-        <IconButton onClick={() => toggleMute()} className={classes.iconButton}>
-          {volume ? (
-            <VolumeUp color="secondary" className={classes.icon} />
-          ) : (
-            <VolumeMute color="secondary" className={classes.icon} />
-          )}
-        </IconButton>
-      </Grid>
+          <IconButton
+            onClick={() => toggleMute()}
+            className={classes.iconButton}
+          >
+            {volume ? (
+              <VolumeUp color="secondary" className={classes.icon} />
+            ) : (
+              <VolumeMute color="secondary" className={classes.icon} />
+            )}
+          </IconButton>
+        </Grid>
+      </FullScreen>
     </Grid>
   )
 }
