fff0a5d0d7ca7efb4cad66fe63c3ea9653112059
Roger BC <diolink.4103@gmail.com>
2021-05-04 12:06:06 -0300

    The query is implemented in hook format

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -77,27 +77,30 @@ const useStyle = makeStyles(Theme => ({
   containerItem: { padding: Theme.spacing(4) },
 }))
 
-const Gallery = ({ artworks, itemType }) => {
+const Gallery = ({ artworksQuery, itemType }) => {
+  const { artworks } = artworksQuery ? artworksQuery : []
   const classes = useStyle()
   return (
     <Grid container direction="row" justify="space-around" wrap="wrap">
-      {artworks.map((artwork, index) => (
-        <Grid
-          item
-          xs={12}
-          sm={6}
-          md={4}
-          lg={3}
-          className={classes.containerItem}
-        >
-          <GalleryItem
-            key={index}
-            itemType={itemType}
-            artwork={artwork}
-            link={'/artwork/show'}
-          />
-        </Grid>
-      ))}
+      {artworks
+        ? artworks.map((artwork, index) => (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              className={classes.containerItem}
+            >
+              <GalleryItem
+                key={index}
+                itemType={itemType}
+                artwork={artwork}
+                link={'/artwork/show'}
+              />
+            </Grid>
+          ))
+        : ''}
     </Grid>
   )
 }
