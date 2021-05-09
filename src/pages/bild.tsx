d0bcc810673ac94678a9a55f5ecbf76abb623735
Roger BC <diolink.4103@gmail.com>
2021-05-08 21:39:53 -0300

    The new Bild page is implemented

---
+++ src/pages/bild.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const BildPage = () => {
+  return (
+    <Layout
+      cois="0.2222"
+      publicKey="0x834fas75fsha87hf38kuk4758"
+      profileImageUrl="https://f8n-ipfs-production.imgix.net/Qme6A7qARnvZsn5RNSuJS8MyZjzzev4afcr6JVJxjciUvB/nft.png"
+      name="Roger"
+    >
+      <div>hol</div>
+      <div>asda</div>
+    </Layout>
+  )
+}
+
+export default BildPage
