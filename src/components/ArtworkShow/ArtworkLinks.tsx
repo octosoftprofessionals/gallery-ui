710f8e34dd0e97f08071ed3df771a4f42babe39f
Adrian Ponce <adrian.ponce@agileengine.com>
2021-05-10 18:05:26 -0300

    Fix max-with

--- src/components/ArtworkShow/ArtworkLinks.tsx
+++ src/components/ArtworkShow/ArtworkLinks.tsx
@@ -1,45 +1,39 @@
 import React from 'react'
-import { Grid} from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
-import LinkButton from '../LinkButton';
+import LinkButton from '../LinkButton'
 
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > *': {
-            marginBottom: theme.spacing(3),
-            width: theme.spacing(16),
-        },
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      marginBottom: theme.spacing(3),
+      width: '100%',
+      maxWidth: 400,
     },
-}));
+  },
+}))
 
-
-const ArtworkView = ({artworkLinks}) => {
-    const classes = useStyles();
-    const preventDefault = (event) => event.preventDefault();
-    return(
-        <Grid
-            container
-            className={classes.root}
-            direction="column"
-            justify="center"
-            alignItems="flex-start"
-            spacing={0}
-            xs={12}
-        >   
-            {
-                artworkLinks.map((elem)=>{
-                    return(
-                        <LinkButton link={elem.link} text={elem.text} icon={elem.icon}/>
-                        
-                    )
-                })
-            }
-        </Grid>
-    )
+const ArtworkView = ({ artworkLinks }) => {
+  const classes = useStyles()
+  const preventDefault = event => event.preventDefault()
+  return (
+    <Grid
+      container
+      className={classes.root}
+      direction="column"
+      justify="center"
+      alignItems="flex-start"
+      spacing={0}
+      xs={12}
+    >
+      {artworkLinks.map(elem => {
+        return <LinkButton link={elem.link} text={elem.text} icon={elem.icon} />
+      })}
+    </Grid>
+  )
 }
 
 export default ArtworkView
--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,79 +1,85 @@
 import React from 'react'
 import { Grid, Typography, Button, Link, Paper } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
-import OpenInNewOutlinedIcon from '@material-ui/icons/OpenInNewOutlined';
+import { makeStyles } from '@material-ui/core/styles'
+import OpenInNewOutlinedIcon from '@material-ui/icons/OpenInNewOutlined'
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > *': {
-            marginBottom: theme.spacing(3),
-            width: theme.spacing(16),
-        },
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      marginBottom: theme.spacing(3),
+      width: '100%',
     },
-    box:{
-        boxSizing: "border-box",
-        width:"60%",
-        backgroundColor: 'white',
-        alignItems: 'center',
-        borderRadius:theme.spacing(2),
-        padding:theme.spacing(0),
+  },
+  box: {
+    boxSizing: 'border-box',
+    width: '60%',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    borderRadius: theme.spacing(2),
+    padding: theme.spacing(0),
+  },
+  paper: {
+    width: '100%',
+    padding: theme.spacing(7),
+    borderRadius: theme.spacing(2),
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    transition: '0.3s all linear',
+    fontFamily: 'Bai Jamjuree Regular',
+    '&:hover': {
+      color: theme.palette.primary.main,
     },
-    paper:{
-        width:"100%",
-        padding:theme.spacing(7),
-        borderRadius:theme.spacing(2),
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent:"space-between",
-        transition:'0.3s all linear',
-        fontFamily:'Bai Jamjuree Regular',
-        '&:hover': {
-            color: theme.palette.primary.main,
-        },
+  },
+  item: {
+    height: '80%',
+  },
+  icon: {
+    fontSize: theme.spacing(8),
+    '&:hover': {
+      color: 'black',
     },
-    item:{
-        height:"80%"
-    },
-    icon: { 
-        fontSize: theme.spacing(8),
-        '&:hover': {
-            color: 'black',
-        }, 
-    },
-    iconText:{ fontSize: theme.spacing(9),},
-    iconPng:{
-        width:theme.spacing(9),
-        height:theme.spacing(9)
-    },
-    text:{
-        fontSize: theme.spacing(5),
-        marginLeft: theme.spacing(3)
-    },
-    grid:{
-        margin:'0',
-        color: theme.palette.primary.main,
-    }
-}));
+  },
+  iconText: { fontSize: theme.spacing(9) },
+  iconPng: {
+    width: theme.spacing(9),
+    height: theme.spacing(9),
+  },
+  text: {
+    fontSize: theme.spacing(5),
+    marginLeft: theme.spacing(3),
+  },
+  grid: {
+    margin: '0',
+    color: theme.palette.primary.main,
+  },
+}))
 
-const LinkButton = ({link, text, icon}) => {
-    const classes = useStyles();
-    const preventDefault = (event) => event.preventDefault();
+const LinkButton = ({ link, text, icon }) => {
+  const classes = useStyles()
+  const preventDefault = event => event.preventDefault()
 
-    return(
-        <Button href={link} underline="none" target="_blank" className={classes.box} component={Link}>
-            <Paper elevation={1} className={classes.paper}>
-                <Grid container xs={8} className={classes.grid}>
-                    <img src={icon} className={classes.iconPng} />
-                    <Typography className={classes.text} variant="h6">
-                        {text}
-                    </Typography>
-                </Grid>
-                <OpenInNewOutlinedIcon className={classes.icon}/>
-            </Paper>
-        </Button>
-    )
+  return (
+    <Button
+      href={link}
+      underline="none"
+      target="_blank"
+      className={classes.box}
+      component={Link}
+    >
+      <Paper elevation={1} className={classes.paper}>
+        <Grid container xs={8} className={classes.grid}>
+          <img src={icon} className={classes.iconPng} />
+          <Typography className={classes.text} variant="h6">
+            {text}
+          </Typography>
+        </Grid>
+        <OpenInNewOutlinedIcon className={classes.icon} />
+      </Paper>
+    </Button>
+  )
 }
 
-export default LinkButton;
+export default LinkButton
