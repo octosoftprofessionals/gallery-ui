42c8948d8fb3364283f5728cd76e685b7f4c2bce
Maria Hayward <hayward.maria@gmail.com>
2021-05-17 14:50:17 -0300

    responsive changes ready for review

--- src/components/BecomeCreatorCover.tsx
+++ src/components/BecomeCreatorCover.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'gatsby'
+/* import { Link } from 'gatsby' */
 import {
     Button,
     Divider,
@@ -11,27 +11,33 @@ import { makeStyles } from '@material-ui/core/styles'
 
 const useStyle = makeStyles(Theme => ({
     root: {
-        padding: Theme.spacing(5, 10),
-        marginBottom: 40,
         alignItems: "center",
+        height: "100%",
         '@media (max-width: 576px)': {
-            padding: Theme.spacing(1, 0),
+            padding: 0,
+            margin: 0,
+            height: "100%",
         },
     },
     container: {
         padding: 20,
         '@media (max-width: 576px)': {
             padding: 0,
+            margin: 0,
+        },
+    },
+    coverTitle: {
 
+        '@media (max-width: 768px)': {
+            fontSize: 36,
+            marginBottom: 20,
         },
     },
     price: {
-        width: "70%",
         fontSize: 18,
         fontWeight: 400,
-        marginBottom: 40,
+
         '@media (max-width: 576px)': {
-            width: "90%",
             marginBottom: 20,
         },
     },
@@ -40,8 +46,8 @@ const useStyle = makeStyles(Theme => ({
         fontSize: 18,
         fontWeight: 400,
         color: "grey",
-        paddingBottom: 40,
-        '@media (max-width: 576px)': {
+
+        '@media (max-width: 768px)': {
             width: "90%",
             marginBottom: 20,
         },
@@ -51,8 +57,14 @@ const useStyle = makeStyles(Theme => ({
         opacity: Theme.palette.action.disabledOpacity[1],
     },
 
-    button: { borderRadius: Theme.shape.borderRadius[2] },
-    textButton: { fontSize: Theme.typography.fontSize[3] },
+    button: {
+        borderRadius: Theme.shape.borderRadius[2],
+        marginTop: 40,
+        marginBottom: 40,
+    },
+    textButton: {
+        fontSize: Theme.typography.fontSize[3],
+    },
     numberTimer: { fontSize: Theme.typography.fontSize[6] },
     link: { textDecoration: 'none' },
     title: {
@@ -66,92 +78,35 @@ const BecomeCreatorCover = () => {
     const classes = useStyle()
     return (
         <Grid container className={classes.container} >
-            <Grid
-                item
-                xs={12}
-                /*  md={11} */
-                container
-                justify="space-around"
-                alignItems="center"
-            >
-
-                <Grid
-                    item
-                    xs={12}
-
-                    container
-                    direction="row"
-                    alignContent="center"
-                    justify="space-between"
-                    className={classes.root}
-                >
-                    <Grid item xs={12} lg={6} container direction="column">
-                        <Typography variant="h3" color="primary">
-                            Become a Creator
-                </Typography>
-
+            <Grid item /* xs={12} */ container justify="space-around" alignItems="center">
+                <Grid item /* xs={12} */ container direction="row" alignContent="center" justify="space-between" className={classes.root}>
+                    <Grid item xs={12} sm={5} md={4}>
+                        <Typography variant="h3" color="primary"><Grid container direction="collum" className={classes.coverTitle} ><span>Introducing</span><span>Community</span><span>Upvote</span></Grid></Typography>
                         <Typography variant="caption" color="primary" className={classes.price}>With the Community Upvote, artists are encouraged to support other artists and to set the stage for a model of community-led curation that puts power in the hands of creators.
                                 In the new creative economy, we all rise up together.</Typography>
                         <Grid item xs={12} md={6}>
-                            {/*   <Link to={linkButtonBid} className={classes.link}> */}
                             <Button variant="contained" fullWidth className={classes.button}>
                                 <Typography variant="button" className={classes.textButton}>
-                                    Place a bid
-              </Typography>
+                                    Join</Typography>
                             </Button>
-                            {/*   </Link> */}
                         </Grid>
                     </Grid>
-                    <Hidden mdDown>
-                        <Divider
-                            orientation="vertical"
-                            flexItem
-                            className={classes.divider}
-                        />
-                    </Hidden>
-                    <Grid item xs={12} lg={5}>
-                        <Grid item xs={12}>
-                            <Typography
-                                variant="button"
-                                color="primary"
-
-                            >
-                                <h2 className={classes.title}>                  How it works
-</h2>
-                            </Typography>
-                            <Typography
-                                /* variant="caption" */
-                                color="primary"
-                                className={classes.titlePrice}
-                            >
-                                If you want to join the creator foundation, set up and verify your profile. Add your name to de Community Upvote
-                                If you are a verified community member, log into your account, review the creator list, and raise up the artist whose work would like to support.
-
-                        </Typography>
-                        </Grid>
-                        <Grid item xs={12} container direction="row" justify="flex-start">
-
-                            <Grid item xs={12} md={5}>
-                                {/* <Link to={linkButtonArtWork} className={classes.link}> */}
-                                <Button variant="outlined" fullWidth className={classes.button}>
-                                    <Typography variant="button" className={classes.textButton}>
-                                        View artwork
-              </Typography>
-                                </Button>
-                                {/*    </Link> */}
-
+                    <Hidden xsDown> <Divider orientation="vertical" flexItem className={classes.divider} /></Hidden>
+                    <Grid item xs={12} sm={5} md={4}>
+                        <Typography variant="button" color="primary"><h2 className={classes.title}>How it works</h2></Typography>
+                        <Typography color="primary" className={classes.titlePrice}>
+                            <Grid container direction="collum">
+                                <span>If you want to join the creator foundation, set up and verify your profile. Add your name to de Community Upvote.</span>
+                                <span>If you are a verified community member, log into your account, review the creator list, and raise up the artist whose work would like to support.</span>
                             </Grid>
+                        </Typography>
+                        <Grid item xs={12} md={6} container direction="row" justify="flex-start">
+                            <Button variant="outlined" fullWidth className={classes.button}>
+                                <Typography variant="button" className={classes.textButton}>
+                                    Learn More </Typography>
+                            </Button>
                         </Grid>
                     </Grid>
-                </Grid>
-                <Grid
-                    item
-                    sm={11}
-                    container
-                    direction="row"
-                    justify="flex-start"
-                    spacing={4}
-                >
                 </Grid>
             </Grid>
         </Grid>
--- src/pages/becomeCreator/index.tsx
+++ src/pages/becomeCreator/index.tsx
@@ -1,28 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-import ArtworkShow from '../../components/ArtworkShow'
 import Layout from '../../components/Layout/Layout'
 import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Typography, Container, Button } from '@material-ui/core'
 import BecomeCreatorCover from '../../components/BecomeCreatorCover'
 import { colors } from '../../components/Styles/Colors'
 
 const useStyle = makeStyles(Theme => ({
   root: {
     flex: 1,
-    margin: Theme.spacing(10, 10),
+    margin: Theme.spacing(5, 10),
     boxSizing: "border-box",
+    height: "100%",
     '@media (max-width: 576px)': {
       padding: Theme.spacing(0, 0),
-      margin: 40,
+      height: "100%",
     },
   },
-/*   containerAsset: {
-    position: 'relative',
-  }, */
 
-/*   containerInfo: { position: 'relative' },
- */}))
+}))
 
 const BecomeCreator = () => {
   const classes = useStyle()
@@ -35,39 +30,6 @@ const BecomeCreator = () => {
     >
       <div className={classes.root}>
         <BecomeCreatorCover></BecomeCreatorCover>
-        {/*         <Container>
-          <Grid
-            item
-            xs={12}
-            container
-            justify="space-around"
-            alignItems="center"
-          >
-            <Grid item xs={12} sm={5} md={4}>
-              <div className={classes.containerAsset}>
-                <h1>
-                  Introducing Community Upvote.
-                </h1>
-                <Typography>
-                  With the Community Upvote, artists are encouraged to support other artists and to set the stage for a model of community-led curation that puts power in the hands of creators.
-              In the new creative economy, we all rise up together.</Typography>
-                <Button variant="contained" color="primary" >Join</Button>
-              </div>
-            </Grid>
-            <div className={classes.line}></div>
-            <Grid item xs={12} sm={5} md={4}>
-              <div className={classes.containerAsset}>
-                <h2>
-                  How it works
-                </h2>
-                <Typography>
-                  If you want to join the creator foundation, set up and verify your profile. Add your name to de Community Upvote
-                  If you are a verified community member, log into your account, review the creator list, and raise up the artist whose work would like to support. </Typography>
-                <Button variant="outlined" color="primary" >Learn more</Button>
-              </div>
-            </Grid>
-          </Grid>
-        </Container> */}
       </div>
     </Layout >
   )
