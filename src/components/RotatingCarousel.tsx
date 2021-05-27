35e38fde433720f29f569853343f8b42437aa92f
Maria Hayward <hayward.maria@gmail.com>
2021-05-27 13:13:52 -0300

    rotationCarousel styles and pogress

--- src/components/RotatingCarousel.tsx
+++ src/components/RotatingCarousel.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
 import HeroAuction from '../components/HeroAuction'
+import {
+    RadioButtonUnchecked,
+    ChevronLeft,
+    ChevronRight
+} from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles(Theme => ({
+    arrowLeft: {
+        position: 'absolute',
+        top: 'auto',
+        left: 'auto',
+
+    },
+    arrowRigt: {
+        position: 'absolute',
+        top: 'auto',
+        right: 'auto',
+    },
+
+}))
 
 const RotatingCarousel = ({ artworksCarousel }) => {
+    const classes = useStyle()
     return (
-        <Carousel>
+        <Carousel PrevIcon={[<ChevronLeft className={classes.arrowLeft} />,
+        <RadioButtonUnchecked />]
+        }
+            NextIcon={[<ChevronRight className={classes.arrowRigt} />,
+            <RadioButtonUnchecked />]
+            }
+        >
             {artworksCarousel ?
                 artworksCarousel.map((artwork, i) => (
                     <HeroAuction
