dbaff64b0ab000553a33bd089ba1faafa86f8ea4
Maria Hayward <hayward.maria@gmail.com>
2021-05-31 18:54:00 -0300

    Changed ButtonConnectWallet to green

--- src/components/Layout/Navbar/ButtonConnectWallet.tsx
+++ src/components/Layout/Navbar/ButtonConnectWallet.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from 'react'
 import { Button, Dialog, Grid, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { colors } from '../../Styles/Colors';
+
 
 import ConnectWalletModal from './ConnectWalletModal'
 
+const useStyle = makeStyles(Theme => ({
+  btnGreen: {
+    background: colors.Green,
+  },
+}))
+
 const ButtonConnectWallet = ({ pathname }) => {
+  const classes = useStyle()
   const [connectWallet, setConnectWallet] = useState(null)
   const handleClose = () => {
     setConnectWallet(null)
@@ -13,6 +23,7 @@ const ButtonConnectWallet = ({ pathname }) => {
       <Grid item xs={4} container justify="flex-end">
         <Button
           variant="contained"
+          className={classes.btnGreen}
           color={pathname === '/creator' ? 'secondary' : ''}
           onClick={() => setConnectWallet(true)}
         >
