04c27e8688c7d40059ae1fff8aff7ff88bd6e89a
Diego <diego.nicolas.gandara@gmail.com>
2021-06-23 16:53:24 -0300

    Change the ROOT variable and url route at getAsset async function.

--- src/services/opensea/core.ts
+++ src/services/opensea/core.ts
@@ -1,16 +1,10 @@
 import axios from 'axios'
 import axiosRateLimit from 'axios-rate-limit'
 import config from '../../config'
-import process from '@types/node' //Not working  
-import process from 'node' // Not working either.
-// https://stackoverflow.com/questions/44416902/how-to-fix-types-node-index-d-ts-is-not-a-module
-//ENV
-const { API_URL } = process.env;
 
+const ROOT = config.API_URL || 'localhost:3000';
 
-const ROOT = API_URL || process.env.NODE_ENV; // Not working
-
-const API_KEY = config.OPENSEA_API_KEY ?? '' // None for now (if so, should come from gallery-api, backend)
+const API_KEY = config.OPENSEA_API_KEY ?? ''
 
 const DEFAULT_HEADERS = {
   'X-API-KEY': API_KEY,
@@ -31,7 +25,7 @@ const get = async (url, queryParams = {}) => {
 
 // https://docs.opensea.io/reference#retrieving-a-single-asset
 export const getAsset = async ({ assetContractAddress, assetTokenId }) => {
-  const url = `${ROOT}//asset${assetContractAddress}/${assetTokenId}/`
+  const url = `${ROOT}/asset/${assetContractAddress}/${assetTokenId}/`
   const res = await get(url)
   const asset = res.data ?? {}
   return asset
