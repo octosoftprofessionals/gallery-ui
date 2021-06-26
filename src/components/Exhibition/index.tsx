0c7b614a83cb0db5e03339deb23b168d4b7843b1
Agustina <agustina@blockinar.com>
2021-06-26 00:26:15 -0300

    mobile exhibition page

--- src/components/Exhibition/index.tsx
+++ src/components/Exhibition/index.tsx
@@ -10,6 +10,12 @@ import useQueryParams from '../../hooks/useQueryParams'
 
 const useStyles = makeStyles(theme => ({
   root: {},
+  title: {
+    padding: theme.spacing(11, 0),
+    '@media (max-width: 576px)': {
+      textAlign: 'center',
+    },
+  },
   button: {
     borderRadius: theme.shape.borderRadius[2],
     padding: theme.spacing(4, 2),
@@ -18,6 +24,14 @@ const useStyles = makeStyles(theme => ({
   },
   containerCollection: {
     padding: theme.spacing(0, 7),
+    '@media (max-width: 576px)': {
+      padding: 0,
+    },
+  },
+  containerGallery: {
+    overflowY: 'scroll',
+    overflowX: 'hidden',
+    height: `${theme.spacing(15)}vh`,
   },
   typographyButton: {
     alignSelf: 'left',
@@ -68,8 +82,8 @@ const Exhibition = () => {
   } = useQuery('featuredItemsQuery', featuredItemsQuery)
   console.log(featuredItems)
   return (
-    <>
-      <Typography variant="h5" display="block" align="center" color="primary">
+    <Grid container justify="center">
+      <Typography className={classes.title} variant="h5" color="primary">
         Superchief Collections, Curated by our staff
       </Typography>
       <Grid item container justify="space-between">
@@ -100,11 +114,11 @@ const Exhibition = () => {
             </div>
           </Paper>
         </Grid>
-        <Grid item xs={12} sm={9}>
+        <Grid item xs={12} sm={9} className={classes.containerGallery}>
           <GalleryExhibition isLoading={isLoading} data={featuredItems} />
         </Grid>
       </Grid>
-    </>
+    </Grid>
   )
 }
 
