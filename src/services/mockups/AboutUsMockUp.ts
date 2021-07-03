5225d7727651a40b867066a0d4b8357718abd74a
Maria Hayward <hayward.maria@gmail.com>
2021-07-03 14:02:09 -0300

    add info to MockUp file

--- src/services/mockups/AboutUsMockUp.ts
+++ src/services/mockups/AboutUsMockUp.ts
@@ -7,13 +7,18 @@ const imgUrls = [
   'https://image.mux.com/OqOt4fV1UKU02PntGC022luD9O7J01JZ701etlf022JIhd6A/thumbnail.jpg',
 ]
 
-const name = ['Roger', 'Maria', 'Agus', 'Adrian']
+const name = [
+  'Chayenne Alvarez',
+  'Luis Miguel',
+  'Ricardo Mollo',
+  'Esteban Furtado',
+]
 
 const opinion = [
-  '“My own success in the crypto art space has afforded me the opportunity to support artists whose work lights me up, which has been just as exciting as advancing my own art practice. There can be an incredible symbiotic relationship between artists and collectors…”',
-  '“Punk has taught me that there’s always a work-around for any obstacle. You just have to push through and make it happen… With NFTs, I want to push at what’s possible myself, and I also watch and see what other people are up to. I’ve been brainstorming, and fuck, there are so many things that we could all do. I’m really excited to see how it all continues to take shape, and be a part of it.”',
-  '“For web3 to flourish, you need creative individuals. Programmers built the technical infrastructure and economic framework, but artists also have a tremendously important role to play. Hopefully, if we can include everyone’s voices, we’ll end up with a stronger community, and better technology that works for everybody.”',
-  '“I want people to experience what I feel when I collect another artist’s work, which is a feeling of interdependence, optimism, and hope. When we participate equally in this system and use our resources to help each other out, we create a mutually beneficial society.”',
+  '“My own success in the crypto art space has afforded me the opportunity to support artists whose work lights me up, which has been just as exciting my own art practice. ',
+  '“Punk has taught me that there’s always a work-around for any obstacle. You just have to push through and make it happen… With NFTs, I want to push at what’s possible myself. ”',
+  '“For web3 to flourish, you need creative individuals. Programmers built the technical infrastructure and economic framework, but artists also have an important role to play. ”',
+  '“I want people to experience what I feel when I collect another artist’s work, which is a feeling of interdependence, optimism, and hope.”',
 ]
 const authorsNames = [
   'Asimov',
@@ -22,6 +27,16 @@ const authorsNames = [
   'Williams',
   'Dickens',
   'DaVinci',
+]
+
+const position = [
+  'Frontend Developer',
+  'Account Manager',
+  'Communications Director',
+  'Senior Brand Designer',
+  'Community Sspecialist',
+  'Cofounder CEO',
+  'Head of Operations',
 ]
 
 const randImgUrl = () => {
@@ -36,6 +51,9 @@ const randOpinion = () => {
 const randName = () => {
   return name[Math.floor(Math.random() * name.length)]
 }
+const randPosition = () => {
+  return position[Math.floor(Math.random() * position.length)]
+}
 
 type ReviewCard = {
   creatorImageUrl: String
@@ -43,6 +61,7 @@ type ReviewCard = {
   opinion: String
   imageUrl: String
   userName: String
+  position: String
 }
 
 function fillReviewCard(size): ReviewCard[] {
@@ -54,8 +73,9 @@ function fillReviewCard(size): ReviewCard[] {
       opinion: randOpinion(),
       imageUrl: randImgUrl(),
       userName: randName(),
+      position: randPosition(),
     })
   }
   return reviewCard
 }
-export default fillReviewCard(4)
+export default fillReviewCard(6)
