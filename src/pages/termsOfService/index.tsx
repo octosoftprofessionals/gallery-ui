1620f38df3f4569a157e23fcbfe882e4922d05b8
Agustina <agustina@blockinar.com>
2021-07-05 22:29:08 -0300

    terms of service page

---
+++ src/pages/termsOfService/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import Terms from '../../components/TermsOfService'
+
+const TermsOfService = () => {
+  return (
+    <Layout padding="0 24px" marginTop="12px">
+      <Terms />
+    </Layout>
+  )
+}
+
+export default TermsOfService
