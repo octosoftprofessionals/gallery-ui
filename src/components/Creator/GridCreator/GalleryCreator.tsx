a5e763f1a169bb1ac69cf9c643f1c833f0f3a689
Jeremy Meyer <jeremypatrickmeyer@gmail.com>
2021-07-14 01:02:45 -0700

    update creator gallery component to use query

--- src/components/Creator/GridCreator/GalleryCreator.tsx
+++ src/components/Creator/GridCreator/GalleryCreator.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
+import { useQuery } from 'react-query'
 
 import { Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
-import GalleryItem from '../../GalleryItem'
+import ArtworkItem from '../../../components/GalleryItem/ArtworkItem'
+import { GalleryItem } from '../../../services/gallery'
 
 const useStyle = makeStyles(Theme => ({
   containerItem: { padding: Theme.spacing(4) },
 }))
 
-const Gallery = ({ artworksQuery, itemType }) => {
-  const { artworks } = artworksQuery ? artworksQuery : []
+const Gallery = ({ type, queryName, queryFunction }: { type: string, queryName: string, queryFunction: () => Promise<GalleryItem[]> }) => {
+  const { data: galleryItems = [] } = useQuery(queryName, queryFunction)
   const classes = useStyle()
   return (
     <Grid container direction="row" justify="space-between" wrap="wrap">
-      {artworks
-        ? artworks.map((artwork, index) => (
+      {galleryItems
+        ? galleryItems.map((galleryItem, index) => (
             <Grid item xs={12} sm={6} md={5} className={classes.containerItem}>
-              <GalleryItem
+              <ArtworkItem
                 key={index}
-                itemType={itemType}
-                artwork={artwork}
-                link={`/artwork?id=${artwork.id}`}
+                galleryItem={galleryItem}
               />
             </Grid>
           ))
