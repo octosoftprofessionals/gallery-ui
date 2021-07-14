1388d9b5b917315696b0cdf42a1362cf5d6b7fa4
Jeremy Meyer <jeremypatrickmeyer@gmail.com>
2021-07-14 01:29:14 -0700

    refactor profile routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -2,6 +2,11 @@ export const artworkPathFrom = (contractAddress: string, tokenId: string) =>
   `/artwork/?contractAddress=${contractAddress}&tokenId=${tokenId}`
 export const biddingPathFrom = (contractAddress: string, tokenId: string) =>
   `/bid/?contractAddress=${contractAddress}&tokenId=${tokenId}`
-export const profilePathFromAddress = address => `/creator/?address=${address}`
+
+export const profilePathFromAddress = (address: string) =>
+  `/creator/?address=${address}`
+export const profilePathFromUsername = (username: string) =>
+  `/creator/?id=${username}`
+
 export const accountPathFrom = (tokenId: string) =>
   `/account/?tokenId=${tokenId}`
