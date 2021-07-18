ad437441f04d5dd23d2cfa8c5e70dca7c244ec5f
Adrian Ponce <adrian.ponce@agileengine.com>
2021-07-18 13:29:25 -0300

    Add types for exhibition

--- src/services/exhibition.ts
+++ src/services/exhibition.ts
@@ -1,27 +1,8 @@
 import axios from 'axios'
 import axiosRateLimit from 'axios-rate-limit'
+import { Exhibition, ExhibitionArtworks } from '../types'
 
-export type Exhibition = {
-    id: Number
-    title: String
-    description: String
-    artist: String
-    priority: Number
-    deliverydate: Date
-}
-
-export type ExhibitionArtworks = {
-    id: Number
-    name: String
-    artist_wallet_address: String
-    asset_contract_address: String
-    asset_token_id: String
-    priority: Number
-    exhibitionid: Number
-}
-
-
-const ROOT =  'http://localhost:3000/v1'
+const ROOT = 'http://localhost:3000/v1'
 
 const http = axiosRateLimit(axios.create(), {
   maxRequests: 5,
@@ -45,65 +26,84 @@ const destroy = async (url, queryParams = {}) => {
   return await http.delete(url, { params: queryParams })
 }
 
-
 /* EXHIBITIONS */
-export const createExhibition = async ( queryParams = {} ) => {
+export const createExhibition = async (
+  queryParams = {}
+): Promise<Exhibition> => {
   const url = `${ROOT}/exhibition`
   const res = await post(url, queryParams)
   const createdExhibition = res.data ?? {}
   return createdExhibition
 }
 
-export const getAllExhibitions = async () => {
+export const getAllExhibitions = async (): Promise<Exhibition[]> => {
   const url = `${ROOT}/exhibition`
   const res = await get(url)
   const allExhibitions = res.data.exhibitions ?? []
   return allExhibitions
 }
 
-export const getOneExhibition = async ({ exhibitionid }) => {
-    const url = `${ROOT}/exhibition/${exhibitionid}`
-    const res = await get(url)
-    const exhibition = res.data ?? res ?? {}
-    return exhibition
+export const getOneExhibition = async ({
+  exhibitionid,
+}): Promise<Exhibition> => {
+  const url = `${ROOT}/exhibition/${exhibitionid}`
+  const res = await get(url)
+  const exhibition = res.data ?? res ?? {}
+  return exhibition
 }
 
-export const deleteOneExhibition = async ({ exhibitionid }) => {
-    const url = `${ROOT}/exhibition/${exhibitionid}`
-    const res = await destroy(url)
-    const deletedExhibition = res.data ?? {}
+export const deleteOneExhibition = async ({
+  exhibitionid,
+}): Promise<Exhibition> => {
+  const url = `${ROOT}/exhibition/${exhibitionid}`
+  const res = await destroy(url)
+  const deletedExhibition = res.data ?? {}
+  return deletedExhibition
 }
 
-export const updateOneExhibition = async ({ exhibitionid }) => {
+export const updateOneExhibition = async ({
+  exhibitionid,
+}): Promise<Exhibition> => {
   const url = `${ROOT}/exhibition/${exhibitionid}`
   const res = await update(url)
   const updatedExhibition = res.data ?? {}
+  return updatedExhibition
 }
 
 /* EXHIBITIONSARTWORKS */
 
-export const addArtworkToExhibition = async ({ exhibitionid }) => {
+export const addArtworkToExhibition = async ({
+  exhibitionid,
+}): Promise<ExhibitionArtworks> => {
   const url = `${ROOT}/exhibition/${exhibitionid}/assets`
   const res = await post(url)
   const addedArtworkToExhibition = res.data ?? {}
   return addedArtworkToExhibition
 }
 
-export const getArtworkByExhibitionId = async ({ exhibitionId }) => {
+export const getArtworkByExhibitionId = async ({
+  exhibitionId,
+}): Promise<ExhibitionArtworks[]> => {
   const url = `${ROOT}/exhibition/${exhibitionId}/assets`
   const res = await get(url)
   const artworkById = res.data.artworks ?? []
   return artworkById
 }
 
-export const deleteArtworkFromExhibition = async ({ exhibitionid, asset_token_id }) => {
+export const deleteArtworkFromExhibition = async ({
+  exhibitionid,
+  asset_token_id,
+}): Promise<ExhibitionArtworks> => {
   const url = `${ROOT}/exhibition/${exhibitionid}/assets/${asset_token_id}`
   const res = await destroy(url)
   const deletedArtworkFromExhibition = res.data ?? {}
-  return deleteArtworkFromExhibition
+  return deletedArtworkFromExhibition
 }
 
-export const updateArtworkFromExhibition = async ({ exhibitionid, artworkid }) => {
+export const updateArtworkFromExhibition = async ({
+  exhibitionid,
+  artworkid,
+}): Promise<ExhibitionArtworks> => {
   const url = `${ROOT}/exhibition/${exhibitionid}/${artworkid}/assets`
   const res = await update(url)
   const updatedArtwork = res.data ?? {}
