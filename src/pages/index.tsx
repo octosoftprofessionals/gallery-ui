739ddb590b226a345a501830a61bd7793c964e84
Roger BC <diolink.4103@gmail.com>
2021-07-20 15:42:39 -0300

    useInfiniteQuery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useQuery } from 'react-query'
+import React, { useState } from 'react'
+import { useInfiniteQuery } from 'react-query'
 
 import ArtworkGrid from '../components/ArtworkGrid'
 import Gallery from '../components/Gallery'
@@ -8,10 +8,14 @@ import Layout from '../components/Layout'
 import Spinner from '../components/Spinner'
 import RotatingCarousel from '../components/RotatingCarousel'
 import EmailPopUp from '../components/EmailPopUp'
-import { featuredItemsQuery } from '../services/gallery'
+import {
+  featuredItemsQuery,
+  featuredInfinitItemsQuery,
+} from '../services/gallery'
 import { GalleryItem } from '../types'
 
 const Home = () => {
+  const [offset, setOffset] = useState(0)
   type featureItemsQueryProps = {
     data: GalleryItem[]
     status: 'idle' | 'error' | 'loading' | 'success'
@@ -19,69 +23,90 @@ const Home = () => {
 
   const {
     data: allFeaturedItems = [],
-    status,
-  }: featureItemsQueryProps = useQuery('FeaturedItems', featuredItemsQuery)
-
-  const heroItem = allFeaturedItems[0]
-  const featuredItems = allFeaturedItems.filter(
-    item => item?.assetId !== heroItem?.assetId
+    isLoading: isAllFeaturedItems,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(
+    'featuredItems',
+    ({ pageParam = 0 }) => featuredInfinitItemsQuery({ offset: pageParam }),
+    {
+      refetchOnWindowFocus: false,
+      getNextPageParam: lastPage => {
+        return lastPage.length >= 20
+      },
+    }
   )
+  const getMore = () => {
+    const newPages = offset + 20
+    setOffset(newPages)
+    fetchNextPage({ pageParam: newPages })
+  }
 
-  const listedItems = featuredItems.filter(i => i.status === 'listed')
-  const reserveItems = featuredItems.filter(i => i.status === 'reserve')
-  // const soldItems = featuredItems.filter(i => i.status === 'sold')
+  // const featuredItems = allFeaturedItems.filter(
+  //   item => item?.assetId !== heroItem?.assetId
+  // )
 
-  const isLoading = status === 'loading'
+  // const listedItems = featuredItems.filter(i => i.status === 'listed')
+  // const reserveItems = allFeaturedItems.pages.filter(
+  //   i => i.status === 'reserve'
+  // )
+  // console.log('reserveItems :>> ', reserveItems)
+  // // const soldItems = featuredItems.filter(i => i.status === 'sold')
+
+  // const isLoading = status === 'loading'
 
   return (
     <Layout>
-      <EmailPopUp></EmailPopUp>
-      {isLoading ? (
+      <EmailPopUp />
+      {/* {isLoading ? (
         <Spinner height="50vh" />
       ) : (
         <RotatingCarousel
           artworksCarousel={featuredItems.slice(0, 2)}
           timeout={1000}
           interval={7000}
-          // theme="dark"
         />
-      )}
+      )} */}
       <ArtworkGrid
         title="Featured artworks"
         titleButton="artworks"
         link="/artworks"
       >
-        {isLoading ? (
+        {isAllFeaturedItems ? (
           <Spinner height="50vh" />
         ) : (
           <Gallery
-            isLoading={isLoading}
-            items={reserveItems}
-            renderItem={(item, index) => (
-              <ArtworkItem key={index} galleryItem={item} />
+            isLoading={isFetching}
+            handleNext={getMore}
+            pages={allFeaturedItems.pages}
+            hasNextPage={hasNextPage}
+            renderItem={(items, index) => (
+              <ArtworkItem key={index} galleryItem={items} />
             )}
           />
         )}
       </ArtworkGrid>
 
-      <ArtworkGrid
+      {/* <ArtworkGrid
         title="Live auctions"
         titleButton="live auctions"
         link="/artworks"
         icon
       >
         {isLoading ? (
-          <Spinner height="60vh" />
+          <Spinner height="50vh" />
         ) : (
           <Gallery
-            isLoading={isLoading}
+            isLoading={isFetching}
             items={listedItems}
+            hasNextPage={hasNextPage}
             renderItem={(item, index) => (
               <ArtworkItem key={index} galleryItem={item} />
             )}
           />
         )}
-      </ArtworkGrid>
+      </ArtworkGrid> */}
     </Layout>
   )
 }
