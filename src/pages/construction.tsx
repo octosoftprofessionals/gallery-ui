cb79445fc211e33894c7ca9e48fbacbf4bf9a1c5
Agustina Ruidiaz <agusruidiaz@hotmail.com>
2021-07-20 21:29:29 -0300

    layout added

--- src/pages/construction.tsx
+++ src/pages/construction.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 import Construction from '../components/Construction'
 
 const constructionPage = () => {
-  return <Construction />
+  return (
+    <Layout padding="0" marginBottom="0" marginTop="15px">
+      <Construction />
+    </Layout>
+  )
 }
 
 export default constructionPage
