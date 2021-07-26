5456ee4894eabbe542e4393f91cfebebe243f24c
Jeremy Meyer <jeremypatrickmeyer@gmail.com>
2021-07-26 11:12:18 -0700

    add node.js polyfills

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,22 @@
+const webpack = require('webpack')
+
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     devtool: 'eval-source-map',
+    resolve: {
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        stream: require.resolve('stream-browserify'),
+        assert: require.resolve('assert'),
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: [require.resolve('buffer'), 'Buffer'],
+      }),
+    ],
   })
 }
