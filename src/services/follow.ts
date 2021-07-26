5a1e25d83d02fc9fc1a1e0f28863d07a0e55ea7d
Diego <diego.nicolas.gandara@gmail.com>
2021-07-26 16:47:06 -0300

    All functions added to /services/follow.ts. WIP: Will need help from a FrontEnd Dev to actually connect this functions to proper component

--- src/services/follow.ts
+++ src/services/follow.ts
@@ -9,21 +9,58 @@ export type FollowData = {
     artist_id: number
 }
 
-const ROOT = config.API_URL || 'http://localhost:3000/v1' 
+const ROOT = config.API_URL || 'http://localhost:3000/v1'
 
 const http = axiosRateLimit(axios.create(), {
   maxRequests: 5,
   perMilliseconds: 1000,
 })
 
+// Helper functions
+
+const get = async (url, queryParams = {}) => {
+  return await http.get(url, { params: queryParams })
+}
+
 const post = async (url, queryParams = {}) => {
   return await http.post(url, { params: queryParams })
+}
+
+const destroy = async (url, queryParams = {}) => {
+  return await http.delete(url, { params: queryParams })
 }
 
 export const createFollow = async (queryParams = {}) => {
     const url = `${ROOT}/follow`
     const res = await post(url, queryParams)
-    const follow = res.data ?? {}
-    console.log("Follow created: ", follow)
-    return follow
+    const createdFollow = res.data ?? {}
+    return createdFollow
+}
+
+export const getOneFolloweeByIdWithAllHisFollowers = async (
+  followee_id: number
+  ) => {
+  const url = `${ROOT}/follow/${followee_id}/followers`
+  const res = await get(url)
+  const followeeWithFollowers = res.data ?? {}
+  return followeeWithFollowers
+}
+
+export const getOneFollowerByIdWithAllHisFollowees = async (
+  follower_id: number
+  ) => {
+  const url = `${ROOT}/follow/${follower_id}/followees`
+  const res = await get(url)
+  const followerWithFollowees = res.data ?? {}
+  return followerWithFollowees
+}
+
+export const unFollow = async (
+  follower_id: number,
+  followee_id: number
+  ) => {
+  const url = `${ROOT}/follow/${follower_id}/${followee_id}`
+  const res = await destroy(url)
+  const deletedFollow = res.data ?? {}
+  return deletedFollow
 }
