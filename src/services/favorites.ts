4ceaf40694e834a26c33e8bef6f018586a29476e
Roger BC <diolink.4103@gmail.com>
2021-07-30 19:32:26 -0300

    Favorite connected on the back-end

--- src/services/favorites.ts
+++ src/services/favorites.ts
@@ -2,64 +2,6 @@ import axios from 'axios'
 import axiosRateLimit from 'axios-rate-limit'
 import config from '../config'
 
-// Types from Database Tables
-export type FavoritesArtworks = {
-  id: number
-  user_id: number
-  user_address: string
-  artwork_id: number
-  priority: number
-}
-
-export type Users = {
-    id: number
-    username: string
-    profile_img_url: string
-    cover_img_url: string
-    public_address: string
-    config: string
-    email: string
-    nonce: number
-    bio: string
-    website: string
-    twitter: string
-    instagram: string
-    discord_id: string
-    youtube: string
-    facebook: string
-    tiktok: string
-    snapchat: string
-}
-
-export type Artworks = {
-    id: number
-    asset_id: number
-    asset_contract_address: string
-    asset_token_id: string
-    title: string
-    description: string
-    image_url: string
-    image_preview_url: string
-    image_thumbnail_url: string
-    image_original_url: string
-    video_url: string
-    creator_username: string
-    creator_imageurl: string
-    creator_address: string
-    owner_username: string
-    owner_image_url: string
-    owner_address: string
-    collection_slug: string
-    collection_payout_address: string
-    collection_name: string
-    collection_image_url: string
-    status: string
-    price_eth: string
-    price_usd: string
-    minted_by_superchief: string
-    is_featured: boolean
-    expiration: string
-}
 const ROOT = config.API_URL || 'http://localhost:3000/v1'
 
 const http = axiosRateLimit(axios.create(), {
@@ -88,7 +30,6 @@ const update = async (url: string, queryParams = {}) => {
 export const createAssociationFavoritesArtworks = async (
   public_address: string,
   asset_id: number
-
 ) => {
   const url = `${ROOT}/favorites/add/${public_address}/${asset_id}`
   const res = await post(url)
@@ -104,7 +45,6 @@ export const getAllFavoritesArtworksFromOneUserByAddress = async (
   const userFavoritesArtworks = res.data ?? {}
   return userFavoritesArtworks
 }
-
 
 export const deleteOneFavoriteArtworkFromOneUser = async (
   asset_id: number,
@@ -125,5 +65,3 @@ export const updatePriorityOfOneFavoriteArtwork = async (
   const updatePriorityFromAFavoriteArtwork = res.data ?? {}
   return updatePriorityFromAFavoriteArtwork
 }
-
-
