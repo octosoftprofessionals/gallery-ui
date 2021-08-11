66e5fdd7f35673b720769ebf0c3c982341fa34a7
Roger BC <diolink.4103@gmail.com>
2021-08-11 13:56:48 -0300

    favorite is connected in myProfile

--- src/components/Creator/GridCreator/GalleryCreator.tsx
+++ src/components/Creator/GridCreator/GalleryCreator.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useQuery } from 'react-query'
 
 import { Box, Grid } from '@material-ui/core'
@@ -7,6 +7,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import ArtworkItem from '../../../components/GalleryItem/ArtworkItem'
 import { GalleryItem } from '../../../services/gallery'
 import Spinner from '../../Spinner'
+import { findeArtwork } from '../../../Utils'
 
 import EmptyAccount from './EmptyAccount'
 
@@ -30,6 +31,17 @@ const Gallery = ({
   const classes = useStyle()
   const { data: ItemsToShow = [], favoriteArtworks = [] } = galleryItems
 
+  const handleFavorite = useCallback(
+    (assetId, status, title) => {
+      const titles = {
+        Items: ItemsToShow,
+        Favorites: favoriteArtworks,
+      }
+      findeArtwork(titles[title], assetId, status)
+    },
+    [ItemsToShow, favoriteArtworks]
+  )
+
   if (!isLoading && ItemsToShow.length === 0 && favoriteArtworks.length === 0) {
     return (
       <Box style={{ padding: 48 }}>
@@ -44,12 +56,24 @@ const Gallery = ({
     <Grid container direction="row" justify="center" wrap="wrap">
       {ItemsToShow.map(galleryItem => (
         <Grid item xs={12} sm={6} md={3} className={classes.containerItem}>
-          <ArtworkItem key={galleryItem.assetId} galleryItem={galleryItem} />
+          <ArtworkItem
+            key={galleryItem.assetId}
+            galleryItem={galleryItem}
+            onFavorite={(assetId, status) =>
+              handleFavorite(assetId, status, 'Items')
+            }
+          />
         </Grid>
       ))}
       {favoriteArtworks.map(galleryItem => (
         <Grid item xs={12} sm={6} md={3} className={classes.containerItem}>
-          <ArtworkItem key={galleryItem.assetId} galleryItem={galleryItem} />
+          <ArtworkItem
+            key={galleryItem.assetId}
+            galleryItem={galleryItem}
+            onFavorite={(assetId, status) =>
+              handleFavorite(assetId, status, 'Favorites')
+            }
+          />
         </Grid>
       ))}
     </Grid>
