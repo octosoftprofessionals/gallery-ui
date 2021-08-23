ff384e06de8349aea9b01d881f68fffeb00b4642
Diego <diego.nicolas.gandara@gmail.com>
2021-08-23 15:58:55 -0300

    Services for Playlist

---
+++ src/services/playlists.ts
@@ -0,0 +1,79 @@
+import { get, post, update, destroy } from './http'
+
+export const createPlaylist = async ({ user_address } : { user_address: string }) => {
+  const url = `/playlist/${user_address}`
+  const res = await post(url)
+  const newPlaylist = res.data ?? {}
+  return newPlaylist
+}
+
+export const getPlaylists = async ( queryParams = {} ) => {
+    const url = '/playlist'
+    const res = await get(url, queryParams)
+    const playlists = res.data ?? []
+    return playlists
+}
+
+export const getOnePlaylistByIdWithRelatedArtworks = async ({
+  playlist_id,
+}: {
+  playlist_id: number
+}) => {
+    const url = `/playlist/${playlist_id}`
+    const res = await get(url)
+    const playlistById = res.data ?? {}
+    return playlistById
+}
+
+export const deleteOnePlaylistByIdWithAssociatedArtworks = async ({
+  playlist_id,
+}: {
+  playlist_id
+: number }) => {
+    const url = `/playlist/${playlist_id}`
+    const res = await destroy(url)
+    const deletedPlaylist = res.data ?? {}
+    return deletedPlaylist
+}
+
+export const updateOnePlaylistById = async ({
+  playlist_id,
+}: {
+  playlist_id: number
+}) => {
+    const url = `/playlist/${playlist_id}`
+    const res = await update(url)
+    const updatedPlaylist = res.data ?? {}
+    return updatedPlaylist
+}
+
+export const updateArtworksPriorities = async () => {
+    const url = '/playlist'
+    const res = await update(url)
+    const updatedArtworkPriorities = res.data ?? {}
+    return updatedArtworkPriorities
+}
+
+export const addArtworkToNewPlaylist = async () => {
+  const url = '/playlist'
+  const res = await post(url)
+  const addedArtworkToNewPlaylist = res.data ?? {}
+  return addedArtworkToNewPlaylist
+}
+
+export const addArtworkToExistingPlaylist = async ({ playlist_id } : { playlist_id }) => {
+    const url = `/playlist/addArtwork/${playlist_id}`
+    const res = await post(url)
+    const addedArtworkToExistingPlaylist = res.data ?? {}
+    return addedArtworkToExistingPlaylist
+}
+
+export const deleteArtworkFromPlaylist = async (
+    {playlist_id, artwork_id} :
+    {playlist_id: number, artwork_id: number}
+) => {
+  const url = `/playlist/${playlist_id}/${artwork_id}/assets`
+  const res = await destroy(url)
+  const deletedArtwork = res.data ?? {}
+  return deletedArtwork
+}
