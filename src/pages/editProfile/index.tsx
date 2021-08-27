d51f93a135e101028d47a455057377f87e27459e
Agustina Ruidiaz <agusruidiaz@hotmail.com>
2021-08-26 23:07:23 -0300

    logged Out State

--- src/pages/editProfile/index.tsx
+++ src/pages/editProfile/index.tsx
@@ -4,12 +4,17 @@ import Layout from '../../components/Layout'
 import EditForm from '../../components/EditForm'
 import { getUser } from '../../services/users'
 import { useMetamaskAccount } from '../../hooks/useAccountStore'
+import NotFound from '../404'
 
 const editProfile = () => {
   const address = useMetamaskAccount()
   const { data: userAccount, isLoading } = useQuery('userQuery', () =>
     getUser({ public_address: address })
   )
+
+  if (!address) {
+    return <NotFound />
+  }
 
   return (
     <Layout>
