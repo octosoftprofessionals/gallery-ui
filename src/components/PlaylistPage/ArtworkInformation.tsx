56e9371e7ce7e84f94d4252cae352ffdc392758f
Agustina Ruidiaz <agusruidiaz@hotmail.com>
2021-09-01 21:34:37 -0300

    styles playlist page

--- src/components/PlaylistPage/ArtworkInformation.tsx
+++ src/components/PlaylistPage/ArtworkInformation.tsx
@@ -10,9 +10,36 @@ import { artworkPathFrom } from '../../config/routes'
 import { GalleryItem } from '../../types'
 
 const useStyle = makeStyles(Theme => ({
+  root: {
+    padding: Theme.spacing(9, 12),
+    marginTop: Theme.spacing(9),
+    backgroundColor: Theme.palette.background.paper,
+    '&:hover': {
+      borderShadow: 'none',
+      transform: 'none',
+      translate: 'none',
+      border: 'none',
+    },
+  },
   containerTitle: { paddingBottom: Theme.spacing(4) },
   text: { marginRight: '1rem' },
-  button: { margin: Theme.spacing(4) },
+  button: {
+    margin: Theme.spacing(4),
+    fontSize: Theme.typography.fontSize[3],
+    borderRadius: Theme.spacing(7),
+    textTransform: 'none',
+    backgroundColor: Theme.palette.buttons.selected,
+    '@media (max-width: 576px)': {
+      margin: Theme.spacing(3, 0),
+      padding: Theme.spacing(3),
+    },
+  },
+  textBtn: {
+    color: Theme.palette.primary.contrastText,
+  },
+  orderText: {
+    color: Theme.palette.primary.dark,
+  },
 }))
 
 const ArtworkInformation = ({
@@ -36,7 +63,7 @@ const ArtworkInformation = ({
   const artworkPath = artworkPathFrom(assetContractAddress, assetTokenId)
 
   return (
-    <Paper>
+    <Paper className={classes.root}>
       <Grid container alignContent="center" justify="center">
         <Grid item xs={7} container justify="center">
           <CardArtwork
@@ -50,24 +77,38 @@ const ArtworkInformation = ({
           item
           xs={12}
           container
+          direction="column"
           justify="center"
+          alignContent="center"
           className={classes.containerTitle}
         >
-          <Typography>{`${title} [${index + 1} - ${
-            arryArtwork.length
-          }]`}</Typography>
+          <Typography align="center" color="primary" variant="h5">
+            {title}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            color="primary"
+            className={classes.orderText}
+            align="center"
+          >{`${index + 1} - ${arryArtwork.length}`}</Typography>
         </Grid>
-        <Grid item xs={7} container alignItems="flex-start" direction="column">
+        <Grid item xs={12} container alignItems="flex-start" direction="column">
           <Grid container direction="row" justify="flex-start">
-            <Typography className={classes.text}>Artist:</Typography>
+            <Typography variant="h6" color="primary" className={classes.text}>
+              Artist:
+            </Typography>
             <Typography variant="caption">{`@${creatorUsername}`}</Typography>
           </Grid>
           <Grid container direction="row" justify="flex-start">
-            <Typography className={classes.text}>Current Owner:</Typography>
+            <Typography color="primary" variant="h6" className={classes.text}>
+              Current Owner:
+            </Typography>
             <Typography variant="caption">{`@${ownerUsername}`}</Typography>
           </Grid>
           <Grid container direction="row" justify="flex-start">
-            <Typography className={classes.text}>Last Price:</Typography>
+            <Typography color="primary" variant="h6" className={classes.text}>
+              Last Price:
+            </Typography>
             <Typography variant="caption">
               {isNaN(parseInt(priceEth))
                 ? '\t —'
@@ -75,13 +116,15 @@ const ArtworkInformation = ({
             </Typography>
           </Grid>
         </Grid>
-        <Grid item xs={9} container justify="flex-end">
+        <Grid item xs={12} container justify="center">
           <Button
             variant="outlined"
             href={artworkPath}
             className={classes.button}
           >
-            <Typography variant="caption">More</Typography>
+            <Typography className={classes.textBtn} variant="button">
+              More
+            </Typography>
           </Button>
         </Grid>
       </Grid>
