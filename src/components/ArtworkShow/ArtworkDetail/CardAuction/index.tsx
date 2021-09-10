46ef716a0421a331aad92477b9922d2ee87f7be6
Roger BC <diolink.4103@gmail.com>
2021-09-10 12:57:02 -0300

    Offer card is created and implemented

--- src/components/ArtworkShow/ArtworkDetail/CardAuction/index.tsx
+++ src/components/ArtworkShow/ArtworkDetail/CardAuction/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import BidArt from './BidArt'
 import BuyNow from './BuyNow'
+import OffertArt from './OffertArt'
 
 const CardAuction = ({
   status,
@@ -27,6 +28,14 @@ const CardAuction = ({
       priceEth={priceEth}
       priceUsd={priceUsd}
       link={biddingLink}
+      ownerAddress={ownerAddress}
+      ownerImageUrl={ownerImageUrl}
+      ownerUsername={ownerUsername}
+    />
+  ) : status === 'none' ? (
+    <OffertArt
+      priceEth={priceEth}
+      priceUsd={priceUsd}
       ownerAddress={ownerAddress}
       ownerImageUrl={ownerImageUrl}
       ownerUsername={ownerUsername}
